e42c7295eae60944d68f1615be7e73097507b430
marcodonatucci <s293556@studenti.polito.it>
2024-06-05 20:10:51 +0200

    first commit

---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Marco Donatucci">
+    <meta name="keywords" lang="it" content="html">
+    <meta name="description" content="Simulazione sito web X">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/icon.png">
+    <link rel="stylesheet" type="text/css" href="css/style_X.css">
+    <title>Xtremely Clear - Home</title>
+</head>
+<body>
+    <header>
+        <img src="images/image1.png" alt="Immagine iniziale">
+        <div>
+            <h1>Xtremely Clear</h1>
+            <blockquote>You can't ban me anymore - Donald Trump</blockquote>
+            <blockquote>L'unico social ammesso nei seminari - Papa Francesco</blockquote>
+            <blockquote>Lo mas guapo que hay - Antonio Banderas</blockquote>
+            <div class="signup-container">
+                <p>Non hai ancora un account?</p>
+                <button class="btn">Registrati ora!</button>
+                <div class="separator"></div>
+                <p>Hai già un account? Accedi adesso!</p>
+                <button class="btn">Accedi</button>    
+            </div>
+        </div>
+    </header>
+    <main>
+        <h3>Cosa troverai qui</h3>
+        <ul class="dropdown-menu">
+            <li>Gestione dell'account semplificata, anche tua nonna riuscirà ad usarlo!</li>
+            <li>Condividi i tuoi pensieri in tutto il mondo!</li>
+            <li>Entra in contatto con nuove persone che hanno i tuoi stessi interessi!</li>
+        </ul>
+    </main>
+    <footer>
+        <p>Copyright &copy; 2024 <a href="mailto:s293556@studenti.polito.it">Elon Musk</a>. Tutti i diritti riservati.</p>
+        <p>Pagina corrente: Xtremely_clear/Home</p>
+    </footer>
+</body>
+</html>
